0db2212215f2d89f92d84f647210251621132ea6
Vihanga Wahundeniya <vwahundeniya@gmail.com>
2026-05-06 16:40:07 +0530

    fix spelling

--- utils/feature_engineering.py
+++ utils/feature_engineering.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
  
  
-@daaclass
+@dataclass
 class CodeMetrics:
     """
     Represents raw metrics for a single function/module.
