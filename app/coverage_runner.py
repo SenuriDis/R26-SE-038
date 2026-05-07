3f70b45693fa6e9853e393e8ecc27a3b516ad587
nisulaRap <nisula.rap@gmail.com>
2026-05-07 20:56:24 +0530

    removed coverage_runner.py as it is no longer needed

--- app/coverage_runner.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-def run_with_coverage():
-    subprocess.run([sys.executable, "-m", "coverage", "run", "-m", "pytest"], capture_output=True)
-    result = subprocess.run([sys.executable, "-m", "coverage", "report"], capture_output=True, text=True)
-    return result.stdout
